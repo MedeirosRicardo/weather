82763d10f23d027060b12dd42559521b7af91a9c
Ricardo Medeiros <43430028+RicardoArninho@users.noreply.github.com>
2019-04-25 22:56:50 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function get(url) {
-    return new Promise(function(req, res) {
-        let httpRequest = new XMLHttpRequest();
+    return new Promise(function (req, res) {
+        var httpRequest = new XMLHttpRequest();
         httpRequest.open('GET', url);
-        httpRequest.onload = function() {
+        httpRequest.onload = function () {
             if (httpRequest.status === 200) {
                 // Return response text if promise is sucessful
                 req(httpRequest.response);
@@ -13,7 +13,7 @@ function get(url) {
         };
 
         // Handle network errors
-        httpRequest.onerror = function() {
+        httpRequest.onerror = function () {
             res(Error('Network Error'));
         };
 
@@ -27,7 +27,7 @@ function successHandler(data) {
     const div = `
         <h2>
         <img
-            src="http://openweathermap.org/img/w/${dataObj.weather[0].icon}.png"
+            src="https://openweathermap.org/img/w/${dataObj.weather[0].icon}.png"
             alt="${dataObj.weather[0].description}"
             width="50"
             height="50"
@@ -49,7 +49,7 @@ function tempToC(kelvin) {
     return (kelvin - 273.15).toFixed(0);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+addEventListener('DOMContentLoaded', function () {
     // apiKey from OpenWeather
     const apiKey = 'f55738c0a1c34ff30d362068829d7fe3';
     // Empty apiKey to test server error
@@ -64,19 +64,19 @@ document.addEventListener('DOMContentLoaded', function() {
         'new+york,us'
     ];
 
-    const urls = locations.map(function(location) {
+    const urls = locations.map(function (location) {
         return `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
     });
 
     // Using async/wait
-    (async function() {
+    (async function () {
         try {
-            let responses = [];
+            var responses = [];
             responses.push(await get(urls[0]));
             responses.push(await get(urls[1]));
             responses.push(await get(urls[2]));
             responses.push(await get(urls[3]));
-            let literals = responses.map(function(response) {
+            var literals = responses.map(function (response) {
                 return successHandler(response);
             });
             weatherDiv.innerHTML = `<h1></h1>${literals.join('')}`;
